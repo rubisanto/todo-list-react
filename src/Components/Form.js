8dc589af81828cfbad0ed5224fd118425c7e8e8e
rubisanto <rubisanto06@gmail.com>
2023-04-03 11:52:29 +0200

    deleteElement + uuid

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import Item from "./Item";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Form(props) {
   const [dataArr, setDataArr] = useState([
-    { txt: "Faire les courses", id: 1 },
-    { txt: "Faire le ménage", id: 2 },
-    { txt: "Faire la vaisselle", id: 3 },
+    { txt: "Faire les courses", id: uuidv4() },
+    { txt: "Faire le ménage", id: uuidv4() },
+    { txt: "Faire la vaisselle", id: uuidv4() },
   ]);
+
+  const deleteElement = (id) => {
+    const filteredState = dataArr.filter((item) => item.id !== id);
+    setDataArr(filteredState);
+  };
 
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
@@ -20,8 +26,15 @@ export default function Form(props) {
 
       <h2>Liste des choses à faire : </h2>
       <ul className="list-group">
-        {dataArr.map((item, index) => {
-          return <Item key={index} txt={item.txt} />;
+        {dataArr.map((item) => {
+          return (
+            <Item
+              key={item.id}
+              txt={item.txt}
+              id={item.id}
+              delFunc={deleteElement}
+            />
+          );
         })}
       </ul>
     </div>
